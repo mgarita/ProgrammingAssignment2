971a6051b342762192029096f8e5fe2cd0d0d12b
Mauricio Garita <mauriciogaritag@gmail.com>
2014-06-22 10:26:16 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## Write a short comment describing this function
 ## In this case the funtion beggins by describing that x is a matrix. For it to be a matrix one has to define how the matrix will be set. Therefore defining y as a matrs
-## the set function allows us to determine a relation between x and y. The get function 
+## the set function allows us to determine a relation between x and y. Then create the get based on a function that will use the varaible assigned for the function. Then there is the equality for the setmatrix and the getmatrix for there to be no noise. 
 
 makeCacheMatrix <- function(x = matrix()) {
         a <- NULL
@@ -12,13 +12,15 @@ makeCacheMatrix <- function(x = matrix()) {
                 a <<- NULL
         }
         get <- function(), x
-        setmatrix <- function(solve) a <<- solve
-        getmatrix <- function (), a
+        setMatrix <- function(solve) a <<- solve
+        getMatrix <- function (), a
         list(set=set, get=get,
-             setmatrix=setmatrix,
-             getmatrix=getmatrix)
+             setMatrix=setMatrix,
+             getMatrix=getMatrix)
 }
 ## Write a short comment describing this function
+## In this case it is solving a matrix by getting the function getmatrix and then assigning solve to get the inverse of the matrix. One it is solved the end of the matrix is set. 
+
 
 cacheSolve <- function(x, ...) {
 a <- x$getmatrix()
